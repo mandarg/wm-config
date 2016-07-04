227352e80807cb0e1a47f984df7d29dddea1f4b5
Mandar Gokhale <mandarg@mandarg.com>
2016-07-04 05:15:20 +0000

    Add readme, separate configs for OSX and Linux

---
+++ README.md
@@ -0,0 +1,15 @@
+# wm-config
+
+Configuration of my window managers of choice on OSX and Linux
+
+## Linux: XMonad
+
+I have XMonad set up on linux with six different workspaces for different purposes. I also
+have [Xmobar](http://projects.haskell.org/xmobar/) set up to display a small amount of system
+data.
+
+## OSX: Hammerspoon
+
+I recently switched from Spectacle to [Hammerspoon](http://www.hammerspoon.org/) because of the configurability, and use it
+with a tallscreen divided into thirds at work. I am still working on increasing the extent of
+customisation.
---
+++ osx/.hammerspoon/init.lua
@@ -0,0 +1,127 @@
+hs.window.animationDuration = 0 -- disable animations
+
+
+-- I don't really need this, just here for an example
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+  hs.notify.new({title="HAMMERSPOON", informativeText="Hello World"}):send():release()
+end)
+
+-- This reloads config, and I think it's in the intro or tutorial
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.notify.new({title="Config loaded"})
+
+
+-- Stuff to manipulate application window sizes and positions starts here.
+
+-- Move window to left half of screen by Ctrl-Alt-Cmd-Left
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Move window to right half of screen by Ctrl-Alt-Cmd-Right
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- This is for my tallscreen which I use as my primary work screen
+-- It is divided into thirds, so...
+
+-- Move application to upper third
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h / 3
+  win:setFrame(f)
+end)
+
+-- Center application, i.e. middle third
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y + (2 * max.h / 3)
+  f.w = max.w
+  f.h = max.h / 3
+  win:setFrame(f)
+end)
+
+-- Move application to bottom third
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "c", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  
+  f.x = max.x
+  f.y = max.y + (max.h / 3)
+  f.w = max.w
+  f.h = max.h / 3
+  win:setFrame(f)
+end)
+
+
+-- Fullscreen application without removing the menu bar.
+-- Apple added this annoying feature which removes the
+-- menu bar (good) and animates it every time you go in
+-- and out of fullscreen (bad). I'll take the loss of
+-- screen space
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "o", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- App specific shortcuts
+-- I only have Aquamacs at the moment, but plan to add
+-- more
+
+hs.hotkey.bind({"cmd", "ctrl"}, "m", function()
+  hs.application.launchOrFocus("Aquamacs")
+end)
